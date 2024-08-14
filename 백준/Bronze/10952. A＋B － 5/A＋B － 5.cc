4098b577918e8e7024c4fafbfdfdb44181057383
림 <132723999+pastarobber@users.noreply.github.com>
2024-08-14 21:15:14 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	//cin >> A >> B;
+	while (cin >> A >> B) {
+		if (A == 0 && B == 0) {
+			break;
+		}
+		cout << A + B << "\n";
+	}
+	return 0;
+}
