76fa51ba8b80a235d31b71aac9637be800a3fdf0
림 <132723999+pastarobber@users.noreply.github.com>
2024-08-17 15:26:53 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N;
+	vector<int> students;
+	bool find_num[30] = { 0, };
+
+	for (int i = 1; i < 29; i++) {
+		cin >> N;
+		find_num[N - 1] = true; // 입력된 수 1로 초기화
+	}
+
+	for (int i = 0; i < 30; i++) {
+		if (!find_num[i]) // 1이 아닐 때
+			cout << i + 1 << endl;
+	}
+}
