c4dc40ea2426a18fbe0e737f90c77998a0073c47
림 <132723999+pastarobber@users.noreply.github.com>
2024-08-19 22:46:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <string>
-#include <vector>
+
 using namespace std;
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+	string N;
+	cin >> N;
 
-	string str;
-	vector<int> alpha(26, -1);
-
-	cin >> str;
-
-	for (int i = 0; i < str.length(); i++) {
-		int offset = str.at(i) - 'a';
-		if (alpha.at(offset) < 0)
-			alpha[offset] = i;
+	for (char x = 'a'; x <= 'z'; x++) {
+		int p = -1;
+		for (char i = 0; N[i]; i++) {
+			if (N[i] == x) {
+				p = i;
+				break;
+			}
+		}
+		cout << p << " ";
 	}
-
-	for (auto& a : alpha)
-		cout << a << " ";
-
 	return 0;
 }
