47f5e8c3244532a63fd73be2850263b89364e03b
림 <132723999+pastarobber@users.noreply.github.com>
2024-08-20 18:25:12 +0900

    Update 단어의 개수.cc

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -14,3 +14,20 @@ int main() {
 
 	return 0;
 }
+-------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+	string str, token;
+	int cnt = 0;
+	getline(cin, str);
+	stringstream ss(str);
+	while (getline(ss, token, ' '))
+		cnt++;
+	cout << cnt;
+	return 0;
+}
